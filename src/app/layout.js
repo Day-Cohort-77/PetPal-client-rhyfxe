d98e89cf1e2364c6fb4c9242aaea098922a438fd
Stephen <peterson.stephen138@gmail.com>
2025-09-29 15:42:00 -0500

    Up to date with latest develop branch

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,9 @@ import "./globals.css";
 import { Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
 import { AuthProvider } from '../contexts/AuthContext';
-import { QueryProvider } from '../providers/QueryProvider';
+import { ThemeProvider } from '../contexts/ThemeContext';
+import DynamicThemeWrapper from '../components/DynamicThemeWrapper';
+import AuthThemeBridge from '../components/AuthThemeBridge';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,13 +26,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Theme appearance="light" accentColor="blue" radius="medium">
-          <QueryProvider>
-            <AuthProvider>
-              {children}
-            </AuthProvider>
-          </QueryProvider>
-        </Theme>
+        <AuthProvider>
+          <ThemeProvider>
+            <AuthThemeBridge>
+              <DynamicThemeWrapper>
+                {children}
+              </DynamicThemeWrapper>
+            </AuthThemeBridge>
+          </ThemeProvider>
+        </AuthProvider>
       </body>
     </html>
   );
